619403d0374cff67464a5f2c834f3cf83c96c3fb
Martin <maiste.martin@gmail.com>
2020-11-24 11:40:57 +0200

    Ülesanded 1 ja 2

---
+++ ül1.php
@@ -0,0 +1,10 @@
+<?php
+// ülesanne 1 Martin Maiste 24/11/2020
+$nimi = "Martin";
+$synniaasta = "1998";
+$tahtkuju = "vähk";
+echo "Minu nimi on ".$nimi.", sündisin aastal ".$synniaasta." ja tähtkuju on ".$tahtkuju."<br>";
+echo "\"It’s My Life\" – Dr. Alban<br>";
+echo "(\\(\\<br>";
+echo "( -.-)<br>";
+echo "o_(\")(\")<br>";
---
+++ ül2.php
@@ -0,0 +1,31 @@
+<?php
+// ülesanne 2 Martin Maiste 24/11/2020
+$i = 25;
+$j = 17;
+
+echo $i. "+" .$j. "=" .($i + $j)."<br>";
+echo $i. "-" .$j. "=" .($i - $j)."<br>";
+echo $i. "*" .$j. "=" .($i * $j)."<br>";
+echo $i. "/" .$j. "=" .($i / $j)."<br>";
+echo $i. "%" .$j. "=" .($i % $j)."<br>";
+
+$mm = 1412;
+$cm = $mm / 10;
+$m = $mm / 1000;
+
+$vormintatud_cm = sprintf('Vastuse: %0.2f', $cm);
+$vormintatud_m = sprintf('Vastuse: %0.2f', $m);
+
+echo $vormintatud_cm . "<br>";
+echo $vormintatud_m . "<br>";
+
+$a = 18;
+$b = 5;
+
+$pindala = $a * $b / 2;
+$ymbermoot = $a + $b + sqrt(($a * $a) + ($b * $b));
+
+$vormintatud_pindala = sprintf('%d', $pindala);
+$vormintatud_ymbermoot = sprintf('%d', $ymbermoot);
+
+echo "Täisnurksel kolmnurgal mille külg a on " .$a. " ja külg b on " .$b. " on pindala " .$vormintatud_pindala. " ja ümbermõõt ".$vormintatud_ymbermoot;
